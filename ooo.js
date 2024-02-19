ba99e7b9f35beb7220f785a41641c17e4b95d845
Dexite07 <126443539+Dexite07@users.noreply.github.com>
2024-02-19 04:54:11 +0000

    almopst

---
+++ ooo.js
@@ -0,0 +1,7 @@
+function Send() {
+    const inputArray = ["User :",  ];
+    const input = document.getElementById("text");
+    const value = input.value;
+    inputArray.push(value);
+    console.log(inputArray);
+  }
